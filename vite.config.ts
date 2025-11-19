154bd1236fb561115469090ce0cb890599eaf46f
Jakub Andrysek <jakub.andrysek03@gmail.com>
2025-11-19 13:32:40 +0100

    feat: add Playwright testing setup and initial test for Wokwi embed

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/wokwi-embed-example/',
+  server: {
+    port: 2222,
+  },
   plugins: [
     react(),
     tailwindcss(),
